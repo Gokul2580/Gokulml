b5ae5ffb51ffac2a4053622e6995c2e3411fcce5
Gokul <143377276+Gokul2580@users.noreply.github.com>
2025-05-15 08:56:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='gokulml',
-    version='0.1',
-    packages=find_packages(),
-    include_package_data=True,
-    package_data={
-        'gokulml': ['programs/*.py'],
-    },
-    description='GokulML Python Program Runner',
+    version='0.1.0',
+    description='Run and copy programs easily with a single import',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Goks',
     author_email='dm@gokulba.in',
-    keywords=['gokul', 'program runner', 'gokulml'],
+    url='https://github.com/Gokul2580/Gokulml',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'gokulml': ['programs/*.py']},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
